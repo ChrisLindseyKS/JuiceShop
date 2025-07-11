44a359b913199b2fed952ed52a928a2d0b1d8fcc
ChrisLindseyKS <services@lindseysoft.com>
2025-07-11 16:35:45 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@
  * Copyright (c) 2014-2025 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
-
 module.exports = {
   extends: 'standard-with-typescript',
   env: {
